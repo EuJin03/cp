f3dfe023b545be0f051179bfad258df05868508b
eugene <73353966+EuJin03@users.noreply.github.com>
2023-05-20 11:22:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,18 +25,43 @@ for x in array:
     gojo = 0
     jogo = 0
     
-    
-# n = int(input())
-# arr = []
-# while n > 0:
-#     arr.append(list(map(str, input().split())))
-#     n-=1
+
+# Question 2
+n = int(input())
+arr = []
+result = {'Startup1': 0, 'Startup2': 0, 'Startup3': 0, 'Startup4': 0, 'Startup5':0}
+while n > 0:
+    arr.append(list(map(str, input().split())))
+    n-=1
     
 # Startup1 1507366 Affordable Fair 7.9
 
-i = 1507366
+i = 0
 SDG = {'CleanEnergy':20, 'QualityEducation':16, 'ZeroHunger':12, 'AffordableHousing':8, 'GenderEquity':4}
 CSR = {'Excellent':9, 'Good':6, 'Fair':3}
 
 for x in arr:
+    total = 0
+    
+    total+= int(x[1]) / pow(10,8) * 40
+    print('1: ', total)
+    for y in SDG:
+        if (x[2] == y):
+            total+=SDG[y]
+    print('2 ', total)
+    for y in CSR:
+        if (x[3] == y):
+            total+=CSR[y]
+    print('3: ', total)
+    total+=float(x[4])*2
+    print('4: ', total)
+    total+=float(x[5])
+    print("5: ", total)
+    
+    result[x[0]] = total
+    
+print(result)
+
+
+    
     
